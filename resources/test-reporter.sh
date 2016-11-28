6b2b5a6631ff8c9c3c1d2b9e4a5bd932ca2ef6bb
Sean Toner <stoner@localhost.localdomain>
2016-11-28 08:21:01 -0500

    Added some options to the script and run the configurator before running tests

--- resources/test-reporter.sh
+++ resources/test-reporter.sh
@@ -1,15 +1,22 @@
-# $1 is path to polarize project, $2 is version
+# $1 is path to polarize project, $2 is version, $3 is project $4 is to enable testcase import
 POLARIZE_PATH=$1
 POLARIZE_VERSION=$2
+OWD=`pwd`
+pushd ${POLARIZE_PATH}
 gradle clean
-gradle shadowJar
-gradle install
+gradle pP 1> /dev/null
+popd
+
+PWD=`pwd`
+echo $PWD
+gradle clean
+gradle build
 
 # Run the Configurator.
 function configure () {
     java -cp $1/build/libs/polarize-${2}-all.jar \
-    org.github.redhatqe.polarize.configuration.Configurator \
-    --project $3 --testcase-importer-enabled $4
+    com.github.redhatqe.polarize.configuration.Configurator \
+    --project $3 --testcase-importer-enabled $4 --base-dir $PWD --edit-config true --project-name testpolarize
 }
 
 # Make sure it's set to project=PLATTP and testcase importer is enabled
@@ -17,12 +24,16 @@ configure ${POLARIZE_PATH} ${POLARIZE_VERSION} "PLATTP" "true"
 
 # run testpolarize test
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5006 \
--cp /home/stoner/Projects/polarize/build/libs/polarize-$2-all.jar:\
+-cp $1/build/libs/polarize-$2-all.jar:\
 /home/stoner/Projects/testpolarize/build/libs/testpolarize-1.0-SNAPSHOT.jar \
 org.testng.TestNG \
 -reporter com.github.redhatqe.polarize.junitreporter.XUnitReporter \
 src/main/resources/suites/test-suite.xml
 
 # Set the config back
-configure ${POLARIZE_PATH} ${POLARIZE_VERSION} "RHEL6" "false"
+for i in `ls -At ~/.polarize/backup`; do
+  print $i
+  break
+done;
+mv $i ~/.polarize/xml-config.xml
 
